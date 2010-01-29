5d09977ff955b4cf230bcfc1c428358db94c2f17
Noah Richards <noahric@microsoft.com>
2010-01-29 00:03:27 -0800

    Added a view creation listener to handle the format map changing.  Without this, changing a setting in the fonts and colors dialog would override the selection brush back to a solid color brush.  Also, bumped the version to 1.1.

--- ViewCreationListener.cs
+++ ViewCreationListener.cs
@@ -5,6 +5,8 @@ using System.Windows.Media;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
+using System.Diagnostics;
+using System;
 
 namespace GradientSelection
 {
@@ -21,52 +23,97 @@ namespace GradientSelection
         /// </summary>
         public void TextViewCreated(IWpfTextView textView)
         {
-            var formatMap = formatMapService.GetEditorFormatMap(textView);
-
-            // Set the gradient brush only if the rich client experience is enabled
-            if (!textView.Options.GetOptionValue(DefaultWpfViewOptions.EnableSimpleGraphicsId))
-            {
-                SetGradientBrush(formatMap);
-            }
-
-            // Track the rich client option changing, to set and clear the gradient brush
-            textView.Options.OptionChanged += (sender, args) =>
-                {
-                    if (args.OptionId == DefaultWpfViewOptions.EnableSimpleGraphicsId.Name)
-                    {
-                        if (!textView.Options.GetOptionValue(DefaultWpfViewOptions.EnableSimpleGraphicsId))
-                        {
-                            SetGradientBrush(formatMap);
-                        }
-                        else
-                        {
-                            ClearGradientBrush(formatMap);
-                        }
-                    }
-                };
+            new FormatMapWatcher(textView, formatMapService.GetEditorFormatMap(textView));
         }
+    }
 
-        void SetGradientBrush(IEditorFormatMap formatMap)
+    internal sealed class FormatMapWatcher
+    {
+        bool inUpdate = false;
+        IEditorFormatMap formatMap;
+        ITextView view;
+
+        static Brush gradientBrush;
+        static Pen gradientBorder;
+
+        static FormatMapWatcher()
         {
             Color highlight = SystemColors.HighlightColor;
             Color darkColor = Color.FromArgb(96, highlight.R, highlight.G, highlight.B);
             Color lightColor = Color.FromArgb(48, highlight.R, highlight.G, highlight.B);
 
+            gradientBrush = new LinearGradientBrush(new GradientStopCollection() { new GradientStop(darkColor, 0.0), 
+                                                                                   new GradientStop(lightColor, 0.5), 
+                                                                                   new GradientStop(darkColor, 1.0) },
+                                                    90.0);
+            gradientBorder = new Pen(SystemColors.HighlightBrush, 1) { LineJoin = PenLineJoin.Round };
+
+            gradientBrush.Freeze();
+            gradientBorder.Freeze();
+        }
+
+        public FormatMapWatcher(ITextView view, IEditorFormatMap formatMap)
+        {
+            this.formatMap = formatMap;
+            this.view = view;
+            
+            this.SetAppropriateBrush();
+
+            formatMap.FormatMappingChanged += (sender, args) => SetAppropriateBrush();
+
+            // Track the rich client option changing, to set and clear the gradient brush
+            view.Options.OptionChanged += (sender, args) =>
+            {
+                if (args.OptionId == DefaultWpfViewOptions.EnableSimpleGraphicsId.Name)
+                {
+                    SetAppropriateBrush();
+                }
+            };
+        }
+
+        void SetAppropriateBrush()
+        {
+            if (inUpdate)
+                return;
+
+            try
+            {
+                inUpdate = true;
+
+                if (!view.Options.GetOptionValue(DefaultWpfViewOptions.EnableSimpleGraphicsId))
+                {
+                    SetGradientBrush();
+                }
+                else
+                {
+                    ClearGradientBrush();
+                }
+            }
+            finally
+            {
+                inUpdate = false;
+            }
+        }
+
+        void SetGradientBrush()
+        {
             // Change the selected text properties to use a gradient brush and an outline pen
             var properties = formatMap.GetProperties("Selected Text");
-            properties[EditorFormatDefinition.BackgroundBrushId] = new LinearGradientBrush(
-                new GradientStopCollection() { new GradientStop(darkColor, 0.0), new GradientStop(lightColor, 0.5), new GradientStop(darkColor, 1.0) },
-                90.0);
-            properties["BackgroundPen"] = new Pen(SystemColors.HighlightBrush, 1) { LineJoin = PenLineJoin.Round };
+            properties[EditorFormatDefinition.BackgroundBrushId] = gradientBrush;
+            properties["BackgroundPen"] = gradientBorder;
             formatMap.SetProperties("Selected Text", properties);
         }
 
-        void ClearGradientBrush(IEditorFormatMap formatMap)
+        void ClearGradientBrush()
         {
             // Clear out the gradient brush and outline pen
             var properties = formatMap.GetProperties("Selected Text");
-            properties.Remove(EditorFormatDefinition.BackgroundBrushId);
-            properties.Remove("BackgroundPen");
+
+            if (properties[EditorFormatDefinition.BackgroundBrushId] == gradientBrush)
+                properties.Remove(EditorFormatDefinition.BackgroundBrushId);
+            if (properties["BackgroundPen"] == gradientBorder)
+                properties.Remove("BackgroundPen");
+
             formatMap.SetProperties("Selected Text", properties);
         }
     }
